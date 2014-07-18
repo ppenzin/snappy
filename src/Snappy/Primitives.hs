6a830b7e00a62aab6b35a97a405cd0a590c865e7
Petr Penzin <penzin-dev@gmail.com>
2014-07-17 22:48:46 -0700

    Initial commit for common primitives

--- src/Snappy/Primitives.hs
+++ src/Snappy/Primitives.hs
@@ -2,5 +2,13 @@
  -}
 module Snappy.Primitives where
 
-{-|TODO Ordered collection of things, where everything depends on previous element -}
+import Snappy
+
+{-|Ordered collection of things, where everything depends on previous element -}
+newtype Chain a = collection :: [a]
+
 {-|TODO Unordered collection of things, where everything can be instantiated in parallel (though not necessarily is) -}
+type Fork a = [a]
+
+--instance (Thing t) => Thing (Chain t) where
+--  instantiate t = map instantiate (collection t)
